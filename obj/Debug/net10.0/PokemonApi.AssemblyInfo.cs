c4370e9660e01a20b3a242b8272452a22f3f99c7
salhadin <salassefa@gmail.com>
2026-01-01 10:53:52 +0300

    oops , i dotn know what is going on

--- obj/Debug/net10.0/PokemonApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/PokemonApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b61085209a382f34078d8a651b6ead14f5b4c421")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
